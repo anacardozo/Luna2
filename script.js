40b446df9827e7c20c10e8b6ab8f01f9a01fd74b
anacardozo <anaflavia.cr.18@gmail.com>
2025-09-07 19:31:47 -0300

    atualizatiom cadastro-professores

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const menu = document.querySelector('.botao-menu');
-const menuLateral = document.querySelector('.menu-lateral');
-const conteudo = document.querySelector('.content');
+const menu = document.querySelector(".botao-menu");
+const menuLateral = document.querySelector(".menu-lateral");
+const conteudo = document.querySelector(".content");
+const navPesquisa = document.querySelector(".nav-pesquisa");
 
-menu.addEventListener('click', () => {
-    menuLateral.classList.toggle('active');
-    conteudo.classList.toggle('active');
-})
-
+menu.addEventListener("click", () => {
+  menuLateral.classList.toggle("active");
+  conteudo.classList.toggle("active");
+  navPesquisa.classList.toggle("active");
+});
