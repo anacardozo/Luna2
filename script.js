b44929114bf247f80570a2c3f98d6764914f1aa7
anacardozo <anaflavia.cr.18@gmail.com>
2025-10-15 20:46:55 -0300

    atualização tela home

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// const menu = document.querySelector(".botao-menu");
-// const menuLateral = document.querySelector(".menu-lateral");
-// const conteudo = document.querySelector(".content");
-// const navPesquisa = document.querySelector(".nav-pesquisa");
+const menu = document.querySelector(".botao-menu");
+const menuLateral = document.querySelector(".menu-lateral");
+const conteudo = document.querySelector(".content");
+const navPesquisa = document.querySelector(".nav-pesquisa");
 
-// menu.addEventListener("click", () => {
-//   menuLateral.classList.toggle("active");
-//   conteudo.classList.toggle("active");
-//   navPesquisa.classList.toggle("active");
-// });
+menu.addEventListener("click", () => {
+  menuLateral.classList.toggle("active");
+  conteudo.classList.toggle("active");
+  navPesquisa.classList.toggle("active");
+});
 
 
 // ALERTAS
